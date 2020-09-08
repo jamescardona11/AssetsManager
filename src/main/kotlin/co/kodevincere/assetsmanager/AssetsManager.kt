d1dffeb4e458bfb6893b11e532118ced5ed0e587
jamescardona11 <jjcardonao@gmail.com>
2020-09-08 17:54:42 -0500

    Refactor project location

---
+++ src/main/kotlin/co/kodevincere/assetsmanager/AssetsManager.kt
@@ -0,0 +1,104 @@
+package co.kodevincere.assetsmanager
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import org.yaml.snakeyaml.Yaml
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.InputStream
+import java.util.*
+import kotlin.collections.ArrayList
+
+const val PUBSPEC_NAME = "pubspec_default.yaml"
+
+class AssetsManager : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        println("Updating assets manager...")
+        val project = e.getData(PlatformDataKeys.PROJECT)
+        val path = Objects.requireNonNull(project)!!.basePath
+        val fileManager = FileManager(path!!)
+
+        fileManager.createDefaultConfigFile()
+
+
+    }
+
+
+    fun readPubspec(path: String, configFile: ConfigFileValues, assetsFiles: ArrayList<AssetsFiles>){
+        println("Updating assets manager...")
+        val pubspec = File(path, PUBSPEC_NAME)
+        val containsKey = hasAssetsAndFontsKey(pubspec)
+        val outPutLines = ArrayList<String>()
+
+
+        val reader = BufferedReader(FileReader(pubspec))
+        var line = reader.readLine()
+        while (line != null){
+            outPutLines.add(line)
+
+            if(containsKey[0]){ //Contains 'assets' key in pubspec
+                if(line.matches(Regex("^ {2}assets:"))){ // start to delete assets
+                    line = reader.readLine()
+                    while(line.matches(Regex("^ {2,}- .*")) || line.matches(Regex("^\\S*$"))){
+                        line = reader.readLine()
+                    }
+                    //add assets to pubspec
+                    outPutLines.addAll(
+                        addNewAssetsToPubspec(
+                            strategy = configFile.pubspecStrategy!!,
+                            assetsFiles = assetsFiles
+                        )
+                    )
+                }
+            }else{
+                if(line.matches(Regex("^ {2}uses-material-design: true"))){
+                    outPutLines.add("  assets:")
+                    //add assets to pubspec
+                    outPutLines.addAll(
+                        addNewAssetsToPubspec(
+                            strategy = configFile.pubspecStrategy!!,
+                            assetsFiles = assetsFiles
+                        )
+                    )
+                }
+            }
+
+            line = reader.readLine()
+        }
+    }
+
+    fun addNewAssetsToPubspec(strategy: String, assetsFiles: ArrayList<AssetsFiles>) : ArrayList<String>{
+        val outPutLines = ArrayList<String>()
+        if(strategy == PubspecStrategy.ASSET.strategy){
+            assetsFiles.filter { it.isAsset() }.forEach { af->
+                outPutLines.add(af.getAssetPrint())
+            }
+        }else{
+            assetsFiles.filter { it.isDirectory() && !it.isDirectoryFonts() }.forEach { af->
+                outPutLines.add(af.getAssetPrint())
+            }
+        }
+
+        return outPutLines
+
+    }
+
+    fun hasAssetsAndFontsKey(pubspec: File): Array<Boolean>{
+        var assets = false
+        var fonts = false
+
+        val yaml = Yaml()
+        val inputStream: InputStream = pubspec.inputStream()
+        val obj = yaml.load<Map<String, Any>>(inputStream)
+
+        if(obj.containsKey("flutter")){
+            val flutterParent = obj["flutter"] as Map<*, *>
+            assets = flutterParent.containsKey("assets")
+            fonts = flutterParent.containsKey("fonts")
+        }
+
+        return arrayOf(assets, fonts)
+    }
+}
---
+++ src/main/kotlin/co/kodevincere/assetsmanager/FileManager.kt
@@ -0,0 +1,163 @@
+package co.kodevincere.assetsmanager
+
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.io.FileWriter
+import java.io.InputStream
+
+private const val FILE_CONFIG_NAME = "assets_manager_config.yaml"
+private const val MAC_OS_DS_STORE = ".DS_Store"
+private const val defValueDart = "\tstatic const String"
+private const val middleValueDart = "= \""
+private const val endValueDart = "\";\n"
+
+class FileManager(val path: String) {
+
+    fun createDefaultConfigFile() {
+        println("Creating assets manager default config...")
+        //Create a default file
+        var finalPath = path
+        if(getSlashCharacter(finalPath)) finalPath += "/"
+        finalPath += FILE_CONFIG_NAME
+
+        val file = File(finalPath)
+
+        //Write default values for file
+        val named = ConfigFileKey()
+
+        val configFile = ConfigFileValues()
+        file.bufferedWriter().use { out ->
+            out.write("name: AssetsManager Config\n")
+            out.write("description: A Config to create a better experience with plugin\n")
+
+            out.newLine()
+            out.newLine()
+            out.write("# Input assets directory\n")
+            out.write("${named.assetsFolder}: ${configFile.assetsFolder}\n")
+
+            out.newLine()
+            out.write("# Output config\n")
+            out.write("${named.defaultFolder}: ${configFile.folderOutput}\n")
+            //out.write("${named.ignoreFonts}: ${configFile.ignoreFontsConfig}\n")
+            out.write("${named.postfixAssets}: ${configFile.postfixAssets}\n") //# folder_name, extension_name, empty or provide personal value
+            out.write("${named.pubspecStrategy}: ${configFile.pubspecStrategy}\n") // # folder or files
+            //out.write("${named.pubspecUpdateFonts}: ${configFile.pubspecUpdateFonts}\n")
+
+            out.newLine()
+            out.write("# Output assets config\n")
+            out.write("${named.nameOfAssetsClass}: ${configFile.nameOfAssetsClass}\n")
+            out.write("${named.nameOfAssetsFile}: ${configFile.nameOfAssetsFile}\n")
+
+            //out.newLine()
+            //out.write("# Output fonts config\n")
+            //out.write("#${named.nameOfFontsClass}: ${configFile.nameOfFontsClass}\n")
+            //out.write("#${named.nameOfFontsFile}: ${configFile.nameOfFontsFile}\n")
+        }
+    }
+
+    fun readConfigFile(): ConfigFileValues {
+        println("Reading assets manager config...")
+
+        //Open a default file
+        val file = File("$path/$FILE_CONFIG_NAME")
+        val yaml = Yaml()
+        val inputStream: InputStream = file.inputStream()
+        val obj = yaml.load<Map<String, Any>>(inputStream)
+
+
+
+        val namedFile = ConfigFileKey()
+        val defaultValues = ConfigFileValues()
+        return ConfigFileValues(
+                assetsFolder = obj[namedFile.assetsFolder] as String? ?: defaultValues.assetsFolder,
+                folderOutput = obj[namedFile.defaultFolder] as String? ?: defaultValues.folderOutput,
+                ignoreFontsConfig = obj[namedFile.ignoreFonts] as Boolean? ?: defaultValues.ignoreFontsConfig,
+                postfixAssets = obj[namedFile.postfixAssets] as String? ?: defaultValues.postfixAssets,
+                pubspecStrategy = obj[namedFile.pubspecStrategy] as String? ?: defaultValues.pubspecStrategy,
+                pubspecUpdateFonts = obj[namedFile.pubspecUpdateFonts] as Boolean? ?: defaultValues.pubspecUpdateFonts,
+                nameOfAssetsClass = obj[namedFile.nameOfAssetsClass] as String? ?: defaultValues.nameOfAssetsClass,
+                nameOfAssetsFile = obj[namedFile.nameOfAssetsFile] as String? ?: defaultValues.nameOfAssetsFile,
+                nameOfFontsClass = obj[namedFile.nameOfFontsClass] as String? ?: defaultValues.nameOfFontsClass,
+                nameOfFontsFile = obj[namedFile.nameOfFontsFile] as String? ?: defaultValues.nameOfFontsFile,
+                fontConfigNull = obj[namedFile.nameOfFontsClass] == null
+        )
+
+    }
+
+    fun readAssets(configFile: ConfigFileValues = ConfigFileValues()): ArrayList<AssetsFiles> {
+        val finalPath = "$path/${configFile.assetsFolder}"
+        val list: ArrayList<AssetsFiles> = ArrayList()
+        File(finalPath).walk().forEach {
+            if(it.name == MAC_OS_DS_STORE) return@forEach
+            list.add(
+                    AssetsFiles(
+                            file = it,
+                            postfix = configFile.postfixAssets,
+                            assetsFolder = configFile.assetsFolder,
+                    )
+            )
+        }
+
+        return list
+    }
+
+    fun createAssetsOutPut(configFile: ConfigFileValues, assetsFiles: ArrayList<AssetsFiles>) {
+        println("Creating output file...")
+        // Create a output dir
+        var finalPath = checkOrAddLibToPath()
+        finalPath = "$finalPath${configFile.folderOutput}"
+        val dir = File(finalPath)
+        dir.mkdirs()
+
+
+        // Create a output file
+        val assetsDartFile = File("$finalPath/${configFile.nameOfAssetsFile!!}.dart")
+        createFileFromZero(assetsDartFile, assetsFiles.filter { it.isAsset() }, configFile.nameOfAssetsClass!!)
+
+        if (!configFile.ignoreFontsConfig!! && !configFile.fontConfigNull) {
+            var fontsDartFile: File? = null
+            if(configFile.nameOfAssetsFile!! != configFile.nameOfFontsFile){
+                fontsDartFile = File("$finalPath/${configFile.nameOfFontsFile!!}.dart")
+            }
+            appendInFile(fontsDartFile
+                    ?: assetsDartFile, assetsFiles.filter { it.isFont() }, configFile.nameOfFontsClass!!)
+        }
+    }
+
+    private fun checkOrAddLibToPath(): String {
+        var finalPath = path
+        if (!finalPath.contains("lib")) {
+            if (getSlashCharacter(finalPath)) finalPath += "/"
+            finalPath += "lib"
+        }
+        if (getSlashCharacter(finalPath)) finalPath += "/"
+
+        return finalPath
+    }
+
+    private fun createFileFromZero(file: File, assetsFiles: List<AssetsFiles>, nameDartClass: String){
+        file.bufferedWriter().use { out ->
+            out.write("class ${nameDartClass.capitalize()} {\n")
+            assetsFiles.forEach {
+                out.write("$defValueDart ${it.outputName} $middleValueDart${it.path}$endValueDart")
+
+            }
+            out.write("}\n\n")
+        }
+    }
+
+    private fun appendInFile(file: File, assetsFiles: List<AssetsFiles>, nameDartClass: String){
+        val fw = FileWriter(file, true)
+        fw.write("class ${nameDartClass.capitalize()} {\n")
+        assetsFiles.forEach {
+            if (it.isFont())
+                fw.write("$defValueDart ${it.outputName} $middleValueDart${it.path}$endValueDart")
+        }
+        fw.write("}\n\n")
+        fw.close()
+    }
+
+    private fun getSlashCharacter(path: String): Boolean {
+        return path[path.lastIndex] != '/'
+    }
+}
